373db1efeaabd91f0c2471697cad102151f7432e
Hamed Bostan <Hamed.Bostan2@gmail.com>
2024-10-26 11:47:42 +0330

    feat: Add project to sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -20,19 +20,19 @@ function App() {
 
   function handleAddProject(projectData) {
     setProjectState((prevState) => {
+      const projectId = Math.random();
       const newProject = {
         ...projectData,
-        id: Math.random(),
+        id: projectId,
       };
 
       return {
         ...prevState,
+        selectedProjectId: undefined,
         projects: [...prevState.projects, newProject],
       };
     });
   }
-
-  console.log(projectState);
 
   let content;
 
@@ -44,7 +44,10 @@ function App() {
 
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectsSidebar onStartAddProject={handleStartAddProject} />
+      <ProjectsSidebar
+        onStartAddProject={handleStartAddProject}
+        projects={projectState.projects}
+      />
       {content}
     </main>
   );
