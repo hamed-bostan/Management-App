e572269f6c37544513d6960a7d4570a2f719a357
Hamed Bostan <Hamed.Bostan2@gmail.com>
2024-10-26 10:17:28 +0330

    feat: Add collecting info from user inputs

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,9 @@
-export default function Button({ children }) {
+export default function Button({ children, ...props }) {
   return (
-    <button className="px-2 py-4 text-xs md:text-base rounded-md bg-stone-700 text-stone-400 hover:bg-stone-600 hover:text-stone-100">
+    <button
+      className="px-2 py-4 text-xs md:text-base rounded-md bg-stone-700 text-stone-400 hover:bg-stone-600 hover:text-stone-100"
+      {...props}
+    >
       {children}
     </button>
   );
